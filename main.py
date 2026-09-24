c1936271c748b2b63f2845cc0f94ed7919d7bb75
xUniverse709 <contactaigrid.id@gmail.com>
2026-09-24 06:38:01 -0400

    Update main.py

--- main.py
+++ main.py
@@ -56,6 +56,7 @@ logging.basicConfig(
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
     level=logging.INFO
 )
+logger = logging.getLogger(__name__)
 
 # ============================================================
 # ENVIRONMENT CONFIG
